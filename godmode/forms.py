9122961576f7d27d028b0f3e41ec0d41b9e47e3c
Churchillk <churchilkodhiambo@gmail.com>
2026-06-15 09:16:35 +0300

    base.html

--- godmode/forms.py
+++ godmode/forms.py
@@ -15,12 +15,37 @@ class GymMembershipForm(forms.ModelForm):
         ]
 
         widgets = {
+            'user': forms.Select(attrs={
+                'class': 'form-control form-select',
+                'id': 'membership_user'
+            }),
+            'membership_plan': forms.Select(attrs={
+                'class': 'form-control form-select',
+                'id': 'membership_plan'
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-control form-select',
+                'id': 'membership_status'
+            }),
             'end_date': forms.DateTimeInput(
-                attrs={'type': 'datetime-local'},
+                attrs={
+                    'class': 'form-control',
+                    'type': 'datetime-local',
+                    'id': 'membership_end_date'
+                },
                 format='%Y-%m-%dT%H:%M'
             ),
+            'payment_method': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'e.g., Visa ending in 1234',
+                'id': 'membership_payment_method'
+            }),
             'last_payment_date': forms.DateTimeInput(
-                attrs={'type': 'datetime-local'},
+                attrs={
+                    'class': 'form-control',
+                    'type': 'datetime-local',
+                    'id': 'membership_last_payment'
+                },
                 format='%Y-%m-%dT%H:%M'
             ),
         }
@@ -28,10 +53,16 @@ class GymMembershipForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['end_date'].input_formats = (
-            '%Y-%m-%dT%H:%M',
-        )
+        self.fields['end_date'].input_formats = ('%Y-%m-%dT%H:%M',)
+        self.fields['last_payment_date'].input_formats = ('%Y-%m-%dT%H:%M',)
 
-        self.fields['last_payment_date'].input_formats = (
-            '%Y-%m-%dT%H:%M',
-        )
+        # Make fields optional
+        self.fields['end_date'].required = False
+        self.fields['payment_method'].required = False
+        self.fields['last_payment_date'].required = False
+
+        # Add help texts
+        self.fields['membership_plan'].help_text = "Select the membership tier"
+        self.fields['status'].help_text = "Current membership status"
+        self.fields['end_date'].help_text = "When does this membership expire? (Optional)"
+        self.fields['payment_method'].help_text = "Credit card or payment method (Optional)"
--- godmode/views.py
+++ godmode/views.py
@@ -23,19 +23,19 @@ class GymMembershipCreateView(CreateView):
     model = HomeModels.GymMembership
     form_class = GymMembershipForm
     template_name = 'GodMode/form.html'
-    success_url = reverse_lazy('membership_list')
+    success_url = reverse_lazy('godmode:membership_list')
 
 
 class GymMembershipUpdateView(UpdateView):
     model = HomeModels.GymMembership
     form_class = GymMembershipForm
     template_name = 'GodMode/form.html'
-    success_url = reverse_lazy('membership_list')
+    success_url = reverse_lazy('godmode:membership_list')
 
 
 class GymMembershipDeleteView(DeleteView):
     model = HomeModels.GymMembership
-    success_url = reverse_lazy('membership_list')
+    success_url = reverse_lazy('godmode:membership_list')
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
